332e5cd4c674b7f8cb4f1c495a6adef5de334084
anickel101 <anson.nickel@gmail.com>
2020-09-16 10:17:44 -0400

    initial move without Move Button

--- index.js
+++ index.js
@@ -2,5 +2,23 @@ document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+  // 1. event listener for a key press
+  // 2. retrieve the input (ArrowRight, ArrowLeft, ArrowDown, ArrowUp)
+  // 3. translate input to "right" "left" "down" "up"
+  // 4. call move(direction)
+  // 5. create new 'li' and append to whatever div ul container on the right
+      // a. ul id="moves-container"
+
+      document.addEventListener("keydown", e => {
+        if (e.code != "ShiftLeft" && e.code != "ShiftRight") {
+          let code = parseCode(e.code);
+          move(code);
+        }
+      })
+
+      function parseCode(input) {
+        console.log(input)
+        return input.slice(5).toLowerCase();
+      }
+
 })
