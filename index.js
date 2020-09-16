fa99f297939494a89197d635c72de6c24f2fe6b5
anickel101 <anson.nickel@gmail.com>
2020-09-16 11:24:13 -0400

    everything works except setInterval

--- index.js
+++ index.js
@@ -10,21 +10,25 @@ document.addEventListener("DOMContentLoaded", function(){
       // a. ul id="moves-container"
 
       document.addEventListener("keydown", e => {
-        if (e.code != "ShiftLeft" && e.code != "ShiftRight") {
-          let code = parseCode(e.code);
-
-          newMoveLi(code);
-
-          // move(code);
+        if (e.code.includes("Arrow")) {
+          newMoveChild(parseCode(e.code));
+        } else if (e.code == "Backspace") {
+          removeLastChild();
         }
       })
+
+      function removeLastChild() {
+        let moveList = document.getElementById("moves-container");
+        let lastMove = moveList.lastChild;
+        lastMove.remove();
+      }
 
       function parseCode(input) {
         console.log(input)
         return input.slice(5).toLowerCase();
       }
 
-      function newMoveLi(code){
+      function newMoveChild(code){
         let moveList = document.getElementById("moves-container");
         let move = document.createElement("li");
         move.textContent = code;
@@ -32,14 +36,28 @@ document.addEventListener("DOMContentLoaded", function(){
       }
 
       let moveButton = document.querySelector("#move-button");
+
       moveButton.addEventListener("click", e => {
         let moveList = document.getElementById("moves-container");
-        if (moveList.children.length > 0) {
-          let lastMove = moveList.lastChild;
-          move(lastMove.innerText);
-          lastMove.remove();
-        }
+        makeMoves(moveList);
       })
 
+      // let timeStepId;
 
+      function makeMoves(movelist){
+        console.log(movelist.children.length);
+        const listLength = movelist.children.length
+        for (let i = 0; i < listLength; i++) {
+          moveLastChild(movelist);
+          console.log(movelist.children.length);
+        }
+      }
+
+      function moveLastChild(movelist){
+        if (movelist.children.length > 0) {
+          let lastMove = movelist.lastChild;
+          move(lastMove.textContent);
+          lastMove.remove();
+        }
+      }
 })
